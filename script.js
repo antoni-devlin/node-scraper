21cf6ab194d2c7bf8557214f01cd3a4b46f9da14
antoni-devlin <antoni.devlin@gmail.com>
2023-03-13 15:35:32 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+import * as cheerio from "cheerio";
+
+function log(msg) {
+  console.log(msg);
+}
+
+async function requestAndLoad(url) {
+  let response = await fetch(url);
+  let html = await response.text();
+  let $ = cheerio.load(html);
+  return $;
+}
+
+let $ = await requestAndLoad("https://www.gold.ac.uk/ba-journalism");
+
+const linkObjects = $("a");
+// this is a mass object, not an array
+
+// Collect the "href" and "title" of each link and add them to an array
+const links = [];
+
+// How is this .each working??
+linkObjects.each((index, element) => {
+  links.push({
+    text: $(element).text(), // get the text
+    href: $(element).attr("href"), // get the href attribute
+  });
+});
+
+log(links);
+
+// // I think allLinks should be an array of all <a> on the page, but it doens't seem like that's what this returns?
+// let allLinks = $("body").find("a");
+
+// for (let link of allLinks) {
+//   log($(link));
+// }
