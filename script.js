7a232cd042d83a1680e8643e9fbe1086fd731772
antoni-devlin <antoni.devlin@gmail.com>
2023-03-15 14:58:16 +0000

    Added fetchSiteMap and BuildUrlList which fetch and dump the gold.ac.uk sitemap into an array

--- script.js
+++ script.js
@@ -4,17 +4,45 @@ function log(msg) {
   console.log(msg);
 }
 
-async function scrape(url) {
-  let response = await fetch(url);
-  let htmlBody = await response.text();
+// Fetch the gold.ac.uk sitemap
+async function fetchSiteMap() {
+  let response = await fetch("https://www.gold.ac.uk/sitemap-en.xml");
+  let xmlBody = await response.text();
 
-  let $ = cheerio.load(htmlBody);
-  return $;
+  let sitemapObject = cheerio.load(xmlBody, {
+    xml: true,
+  });
+  return sitemapObject;
 }
 
-let result = await scrape("https://www.gold.ac.uk/ba-journalism");
+async function buildUrlList() {
+  let sitemapObj = await fetchSiteMap();
+  let xmlUrls = sitemapObj("loc");
+  let urlList = [];
 
-let links = result("a");
+  for (let url of xmlUrls) {
+    let link = url.children[0].data;
+    link = link.replace(/^.*\/\/[^\/]+/, "");
+    urlList.push(link);
+  }
+  return urlList;
+}
+
+let sitemap = await buildUrlList();
+
+log(sitemap.length);
+
+// async function scrape(url) {
+//   let response = await fetch(url);
+//   let htmlBody = await response.text();
+
+//   let $ = cheerio.load(htmlBody);
+//   return $;
+// }
+
+// let result = await scrape("https://www.gold.ac.uk/ba-journalism");
+
+// let links = result("a");
 
 // for (let link of links) {
 //   log(link.attribs.href);
